5bf201e3bee67410d7644c7db54c380953e38a8e
Spola1 <1111.22224@mail.ru>
2020-11-16 17:55:28 +0300

    lesson_8

--- wagonpass.rb
+++ wagonpass.rb
@@ -1,6 +1,17 @@
 class WagonPass < Wagon
-  def initialize(id)
-    super
+  attr_reader :free_seats
+  def initialize(id, number_of_places)
+    super(id)
     @type = 'Пассажирский'
+    @number_of_places = number_of_places
+    @free_seats = number_of_places
+  end
+
+  def take_seat
+    @free_seats -= 1
+  end
+
+  def occupied_seats_quantity
+    @number_of_places - @free_seats
   end
 end
