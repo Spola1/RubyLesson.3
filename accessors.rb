8fedec80679de5ed2943a7f5e568de12d6779b9e
Spola1 <1111.22224@mail.ru>
2020-11-24 11:19:05 +0300

    lesson10

--- accessors.rb
+++ accessors.rb
@@ -8,8 +8,8 @@ module Accessors
   module ClassMethods
     def attr_accessor_with_history(*names)
       names.each do |name|
-        var_name = "@#{name}".to_sym
-        name_history = "@#{name}_history".to_sym
+        var_name = "#{name}".to_sym
+        name_history = "#{name}_history".to_sym
 
         define_method(name) { instance_variable_get(var_name) }
         define_method("#{name}=".to_sym) do |value|
@@ -22,13 +22,13 @@ module Accessors
         end
 
         define_method("#{name}_history") do
-          instance_variable_get("@#{name}_history".to_sym)
+          instance_variable_get("#{name}_history".to_sym)
         end
       end
     end
 
     def strong_attr_accessor(attr_name, attr_class)
-      var_name = "@#{attr_name}".to_sym
+      var_name = "#{attr_name}".to_sym
       define_method(attr_name) { instance_variable_get(var_name) }
 
       define_method("#{attr_name}=".to_sym) do |value|
